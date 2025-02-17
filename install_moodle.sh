408de322e2df6314ad96641b2b20fdf31f9fc088
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-02-17 19:17:28 +0200

    Update install_moodle.sh

--- install_moodle.sh
+++ install_moodle.sh
@@ -163,11 +163,22 @@ server {
       alias /var/www/moodledata/;
     }
 
-    location ~ [^/].php(/|$) {
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location ~ [^/]\.php(/|$) {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php8.3-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         include fastcgi_params;
+    }
+
+    # Improve Moodle performance
+    location ~* \.(js|css|png|jpg|jpeg|gif|ico|svg)$ {
+        try_files $uri =404;
+        expires max;
+        log_not_found off;
     }
 }
 EOF
