3f5a174e5cb4d3c0eee8d910355d678528e65b25
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-04-21 17:51:05 +0200

    Update install_moodle_apache.sh

--- install_moodle_apache.sh
+++ install_moodle_apache.sh
@@ -146,20 +146,34 @@ sudo chown -R www-data:www-data /var/quarantine
 sudo cat <<EOF > /etc/apache2/sites-available/moodle.conf
 
 <VirtualHost *:80>
- DocumentRoot /var/www/html
- ServerName $WEBSITE_NAME
- ServerAlias www.$WEBSITE_NAME
- ServerAdmin admin@$WEBSITE_NAME
- 
- <Directory /var/www/html>
- Options -Indexes +FollowSymLinks +MultiViews
- AllowOverride All
- Require all granted
- </Directory>
+  # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        ServerName \$WEBSITE_NAME
 
- ErrorLog \${APACHE_LOG_DIR}/error.log
- CustomLog \${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+ </VirtualHost>
 EOF
 
 a2dissite 000-default.conf
