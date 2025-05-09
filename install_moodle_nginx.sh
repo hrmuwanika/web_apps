fbe1bc2c0810e67dd3ceefe51faee371075b8171
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-05-09 22:40:22 +0200

    Update install_moodle_nginx.sh

--- install_moodle_nginx.sh
+++ install_moodle_nginx.sh
@@ -251,7 +251,7 @@ global \$CFG;                                // This is necessary here for PHPUn
 
 \$CFG->slasharguments = 0; 
 \$CFG->preventexecpath = true;
-\$CFG->wwwroot   = 'http://\$WEBSITE_NAME';
+\$CFG->wwwroot   = "http://\$WEBSITE_NAME";
 \$CFG->dataroot  = '/var/www/moodledata';
 \$CFG->directorypermissions = 0777;
 \$CFG->admin = 'admin';
