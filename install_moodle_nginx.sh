286239e3ee6929fcdbee82bf5ae145e5e5faafd7
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-05-16 20:21:13 +0200

    Update install_moodle_nginx.sh

--- install_moodle_nginx.sh
+++ install_moodle_nginx.sh
@@ -154,17 +154,23 @@ server {
     # Log files
     access_log /var/log/nginx/moodle.access.log;
     error_log /var/log/nginx/moodle.error.log;
-    
+
     client_max_body_size 100M;
+    autoindex off;
+
     location / {
-       try_files \$uri \$uri/ /index.php?$args; 
+        try_files \$uri /index.php?\$query_string;
+    }
+
+    location @rewrite {
+        rewrite ^/(.*)$ /index.php?q=\$1;
     }
     
-    location ~ \.php$ {
-    include snippets/fastcgi-php.conf;
-    fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
-    fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    include fastcgi_params;
+    location ~ '\.php$|^/update.php' {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php8.3-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
     }
 
     location /dataroot/ {
@@ -172,26 +178,19 @@ server {
       alias /var/www/moodledata/;
     }
 
-    location ~ /\.ht {
-        deny all;
-    }
-    
-    location = /favicon.ico {
-        log_not_found off;
-        access_log off;
+    location ~* /sites/.*/files/styles/ {
+        try_files \$uri @rewrite;
     }
 
-    location = /robots.txt {
-        allow all;
-        log_not_found off;
-        access_log off;
-    }	
-    
+    location ~ ^/sites/.*/files/ {
+        try_files \$uri @rewrite;
+    }
+
     location ~* \.(js|css|png|jpg|jpeg|gif|ico|svg)$ {
         expires max;
         log_not_found off;
-    }	  
-}
+     }
+ }
 EOF
 
 sudo rm /etc/nginx/sites-available/default
