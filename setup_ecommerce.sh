d54cf1c058455df54080d3e0d65fbec9aae9caa2
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-06-13 19:35:37 +0200

    Update setup_ecommerce.sh

--- setup_ecommerce.sh
+++ setup_ecommerce.sh
@@ -29,7 +29,7 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 sudo apt update
 
-sudo apt install -y postgresql-16 postgresql-contrib
+sudo apt install -y postgresql-16 postgresql-client postgresql-contrib
 
 sudo systemctl start postgresql 
 sudo systemctl enable postgresql
