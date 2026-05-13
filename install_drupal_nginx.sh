2b7992c2979d2e8706c4ef2b8536dc690f7f2fe0
Henry Robert Muwanika <hrmuwanika@gmail.com>
2026-05-13 15:15:12 +0200

    Update install_drupal_nginx.sh

--- install_drupal_nginx.sh
+++ install_drupal_nginx.sh
@@ -41,7 +41,8 @@ echo "
 # # PHP 8.4 installation
 #--------------------------------------------------"
 sudo apt install -y php8.4 php8.4-common php8.4-cli php8.4-intl php8.4-xmlrpc php8.4-zip php8.4-gd php8.4-tidy php8.4-mbstring php8.4-curl php-pear \
-php8.4-dev php8.4-bcmath php8.4-pspell php8.4-ldap php8.4-soap php8.4-gmp php8.4-imagick php8.4-fpm php8.4-redis php8.4-apcu php8.4-mysql php8.4-xml 
+php8.4-dev php8.4-bcmath php8.4-pspell php8.4-ldap php8.4-soap php8.4-gmp php8.4-imagick php8.4-fpm php8.4-redis php8.4-apcu php8.4-mysql php8.4-xml \
+php8.4-pgsql
 
 sudo systemctl start php8.4-fpm
 sudo systemctl enable php8.4-fpm
