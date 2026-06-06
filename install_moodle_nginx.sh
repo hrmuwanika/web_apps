f57a2b234378d40e31034c2f0b2cb14ef3402a65
Henry Robert Muwanika <hrmuwanika@gmail.com>
2026-06-06 16:34:04 +0200

    Update install_moodle_nginx.sh

--- install_moodle_nginx.sh
+++ install_moodle_nginx.sh
@@ -185,13 +185,12 @@ server {
     # Deny access to internal files and dataroot
     location /dataroot/ {
       internal;
-      alias /var/moodledata/;
     }
 
     # Pass PHP scripts to FastCGI server
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php8.x-fpm.sock; 
+        fastcgi_pass unix:/var/run/php/php8.3-fpm.sock; 
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         include fastcgi_params;
     }
