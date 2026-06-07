6f388b67549448978105f192cdc40dd4443822d7
Henry Robert Muwanika <hrmuwanika@gmail.com>
2026-06-07 22:04:28 +0200

    Update install_moodle_nginx.sh

--- install_moodle_nginx.sh
+++ install_moodle_nginx.sh
@@ -73,7 +73,7 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 
 sudo apt install -y php8.3 php8.3-cli php8.3-common php8.3-apcu php8.3-mbstring php8.3-gd php8.3-intl php-pear php8.3-xml php8.3-soap php8.3-bcmath php8.3-mysql php8.3-zip \
-php8.3-curl php8.3-tidy php8.3-imagick php8.3-gmp php8.3-fpm php8.3-xmlrpc php8.3-pspell php8.3-exif php8.3-ldap php8.3-pgsql php8.3-sodium
+php8.3-curl php8.3-tidy php8.3-imagick php8.3-gmp php8.3-fpm php8.3-xmlrpc php8.3-pspell php8.3-exif php8.3-ldap php8.3-pgsql
 
 echo "
 #----------------------------------------------------
